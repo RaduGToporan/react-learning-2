c42fd36aaad6a7ac1b3f8be2d796d5c14293d7eb
RaduGToporan <radutoporan@yahoo.com>
2021-06-03 16:06:16 +0300

    windowWidth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import TodoList from './components/TodoList';
 function App() {
   return (
     <div className='todo-app'>
-      <TodoList/>
       <Posts/>
     </div>
   );
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,55 +1,20 @@
 import React, {useState, useEffect} from 'react'
 
 function Posts(){
-    const [resourceType, setResourceType] = useState('posts');
-    const [messages, setMessages] = useState([]);
-    const [count, setCount] = useState(0);
-    const [items, setItems] = useState([])
+    const [windowWidth, setwindowWidth] = useState(window.innerWidth);
 
-    console.log('render outside useEffect');
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-        .then(response => response.json())
-        .then(json => setItems(json))
-        setCount(count+1);
-        //setMessages([...messages, 'Afisare']);
-    }, [resourceType]);
-
-    const renderMessages = () => {
-        return(
-            <div>
-            {messages.map((message, index) => (
-                <h1 key={index}>{message}</h1>
-            )
-            )}
-            </div>
-        )
+    const handleResize = () => {
+        setwindowWidth(window.innerWidth)
     }
 
-    const renderCounter = () => {
-        let result = [];
-        for (let i=0; i<count; i++)
-        result.push(<h1 key={i}>Afisare</h1>);
-        return result;
-    }
+    useEffect( () => {
+        window.addEventListener('resize', handleResize)
+    }, [])
 
     return (
-        <>
-        <br/>
         <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
+        {windowWidth}
         </div>
-        <h1>{resourceType}</h1>
-        {renderCounter()}
-        {/* {renderMessages()} */}
-
-        {items.map(item => {
-            return <pre>{JSON.stringify(item)}</pre>
-        })}
-        </>
     )
 }
 
