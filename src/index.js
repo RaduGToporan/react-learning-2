d9444bcc495c52b10096790ad9538f7825179759
RaduGToporan <radutoporan@yahoo.com>
2021-06-03 22:28:23 +0300

    map ListItems

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
- const myFirstElement =<p>Hello React !!!</p>
- const appContainer = document.getElementById('root')
+const myFirstElement =<p>Hello React !!!</p>
+const appContainer = document.getElementById('root')
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map( (numbers) =>
+  <li>{numbers}</li>
+ )
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +15,7 @@ ReactDOM.render(
      {myFirstElement}
      {myFirstElement}
     <p>New Text</p> 
+    <ul>{listItems}</ul>
   </React.StrictMode>,
   appContainer
 );
